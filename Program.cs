c971551e89bfdd1b541ed75a183354628ab7cebf
Jan Macek <honzamacek1@gmail.com>
2020-11-24 17:45:04 +0100

    Bugfix , FINALLY WORKING

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,15 +21,30 @@ namespace NC2
             }
             return -1;
         }
+
+        static bool checkForMissingData(string line, char separator)
+        {
+            int firstOccurence = line.IndexOf(separator);
+            char prevChar = line[firstOccurence];
+            for(int i = firstOccurence+1; i<line.Length;i++)
+            {
+                if(line[i]==prevChar&&prevChar==separator&&line[i+1]!='*'){return true;}
+                prevChar = line[i];
+            }
+            return false;
+        }
         static int Main(string[] args)
         {
            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
             //Define root path
             path = path.Substring(6);
 
-            Console.WriteLine("Ahoj, napis prosim nazev souboru s neopracovanymi GPS daty (i s priponou).\nNazev souboru musi byt ve stejnem adresari jako je tento program.\n\nNazev souboru:");
             //Get the input file name
+            Console.WriteLine("Ahoj, napis prosim nazev souboru s neopracovanymi GPS daty (i s priponou).\nNazev souboru musi byt ve stejnem adresari jako je tento program.\n\nNazev souboru:");
             string filenameIn = Console.ReadLine();
+
+
+            bool GSV = false;
 
             //Chybova hlaska
             while (!File.Exists(Path.Combine(path, filenameIn)))
@@ -37,6 +52,17 @@ namespace NC2
                 Console.WriteLine("Program nemuze najit soubor. Ujisti se ze jsi zadal nazev souboru spravne (s priponou) a ze je ve stejne slozce jako tento program.\nZkus napsat nazev souboru znovu:");
                 filenameIn = Console.ReadLine();
             }
+
+            Console.WriteLine("Budeš si přát vypsat i GSV zprávy (SNR data atd.)?\n(A/N):");
+
+            while(true)
+            {
+                string gsvString = Console.ReadLine();
+                if(gsvString.ToUpper() == "A"){GSV = true;break;}
+                if(gsvString.ToUpper() == "N"){GSV = false;break;}
+                Console.WriteLine("Neocekavany vstup. \nOdpovidej pouze 'a' nebo 'n':");
+            }
+
 
             //Get the output name
             Console.WriteLine("Nyni napis nazev vystupniho souboru (bez pripony, bude to .csv) a nebo jenom zmackni enter:");
@@ -76,83 +102,99 @@ namespace NC2
                 //Attrifutes format variables
                 int nmeaAttIndex=0;
                 string nmeaAttStr = "";
-                int nmeaAttComma = 0;
+                int nmeaAtt = 0;
 
                 //Check attributes format
                 if(lines[50].Substring(0,4)=="NMEA")
                 {
-                    nmeaAttIndex = 5;
                     nmeaAttStr = "NMEA,";
-                    nmeaAttComma = 1;
+                    nmeaAttIndex = nmeaAttStr.Length;
+                    nmeaAtt = 1;
                 }
 
                     foreach (string line in lines)
                     {
-                        
+                        //Detect the GGA messages
                         if (line.StartsWith(nmeaAttStr+"$GPGGA")|| line.StartsWith(nmeaAttStr+"$GLGGA")|| line.StartsWith(nmeaAttStr+"$GNGGA"))
                         {
                             string edited;
                             int coordCommaIndex = 0;
                             float coord = 0;
                             string coordString;
-                        float result;
+                            float result;
                             
+                            //Replace commas to make working with decimal separators easier
                             edited=line.Replace(',',';');
                             
-                            if(line[7]==';')
-                                continue;
-                            edited=line.Insert(getNthIndex(line,';',1+nmeaAttComma)+3, ":");
-                            edited=edited.Insert(getNthIndex(line,';',1+nmeaAttComma)+5, ":");
+                            //Discard empty messages
+                            if(checkForMissingData(edited,';')){continue;}
 
-                            coordCommaIndex = getNthIndex(edited,';',3);
+                            //Improve readability of the timestamp attribute
+                            edited=edited.Insert(getNthIndex(edited,';',1+nmeaAtt)+3, ":");
+                            edited=edited.Insert(getNthIndex(edited,';',1+nmeaAtt)+6, ":");
+
+
+                            ///LATITUDE
+
+
+                            //Get the 3rd (latitude) attribute
+                            coordCommaIndex = getNthIndex(edited,';',2+nmeaAtt);
                             coordString=edited.Substring(coordCommaIndex+1,11);
-                            if(coordString[1]!=';')
-                            {
-                                //coordString=coordString.Replace('.',',');
-                                Console.WriteLine(coordString.Substring(0,2));
-                                coord=float.Parse(coordString.Substring(0,2));
+
+                            //Get the deg. part of coordinate
+                            coord=float.Parse(coordString.Substring(0,2));
+
+                            //Get the minute part of coordinate
                             if(!float.TryParse(coordString.Substring(2), out result))
                             {
                                 if(!float.TryParse(coordString.Substring(2).Replace('.',','), out result))
                                     return -1;
-                    }
+                            }
 
+                            //Convert minutes to degrees and add to the deg. part
                             coord+=(result/60);
 
-                            commaIndex = getNthIndex(edited,';',3);
-                            coordString=edited.Substring(commaIndex+1,11);
-                            if(coordString[1]!=';')
-                            {
-                                //coordString=coordString.Replace('.',',');
-                                coord=float.Parse(coordString.Substring(0,2));
-                                coord+=float.Parse(coordString.Substring(2))/60;
-                                edited = edited.Replace(coordString, coord.ToString());
+                            //Replace the non-converted string
+                            edited = edited.Replace(coordString, coord.ToString());
 
 
-                                commaIndex = getNthIndex(edited,';',5);
-                                coordString=edited.Substring(commaIndex+1,12);
-                                //coordString=coordString.Replace('.',',');
-                                coord=float.Parse(coordString.Substring(0,3));
-                            //Console.WriteLine(float.Parse(coordString.Substring(3)));
-                            //coord=(float.Parse(coordString.Substring(3))/60);
+                            ///LONGITUDE
+
+
+                            //Get the 5th attribute (longitude) index location
+                            coordCommaIndex = getNthIndex(edited,';',4+nmeaAtt);
+                            //Isolate the attribute
+                            coordString=edited.Substring(coordCommaIndex+1,12);
                             
+                            //Get the deg. part of coordinate
+                            coord=float.Parse(coordString.Substring(0,3));
+                            
+                            //Get the minute part of coordinate
                             if(!float.TryParse(coordString.Substring(3), out result))
                             {
                                 if(!float.TryParse(coordString.Substring(3).Replace('.',','), out result))
                                     return -1;
                             }
-                            coord+=(result/60);
-                                edited = edited.Replace(coordString, coord.ToString());
 
+                            //Convert minutes to degrees and add to the deg. part
+                            coord+=(result/60);
+
+                            //Replace the non-converted string
+                            edited = edited.Replace(coordString, coord.ToString());
+
+                            //Write into the output file
                             outputFile.WriteLine(edited);
-                                
-                            }
                         }
-                }
+
+                        if (line.StartsWith(nmeaAttStr+"$GPGSV")|| line.StartsWith(nmeaAttStr+"$GLGSV")|| line.StartsWith(nmeaAttStr+"$GNGSV"))
+                        {
+
+                        }
+
+                    }
+            }
 
                 Console.WriteLine("Hotovo! Ocisteny soubor byl vytvoren.");
-
-            }
 
             Console.WriteLine("\nPro ukonceni programu stiskni enter...");
             Console.ReadLine();
